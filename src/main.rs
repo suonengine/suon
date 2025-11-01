5acb052864599fc375f470c990eba14aeb6d833a
ramon-bernardo <ramonbernardo.contato@gmail.com>
2025-11-01 00:28:00 -0300

    feat(app): add bevy minimal app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use bevy::{
+    app::ScheduleRunnerPlugin,
+    diagnostic::{
+        DiagnosticsPlugin, EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin,
+        LogDiagnosticsPlugin, SystemInformationDiagnosticsPlugin,
+    },
+    log::LogPlugin,
+    prelude::*,
+};
+use std::time::Duration;
+
+const N_THREADS: usize = 2;
+const EVENT_LOOP: f64 = 1.0 / 60.0;
+const FIXED_EVENT_LOOP: f64 = 1.0 / 20.0;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            MinimalPlugins
+                .set(TaskPoolPlugin {
+                    task_pool_options: TaskPoolOptions::with_num_threads(N_THREADS),
+                })
+                .set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
+                    EVENT_LOOP,
+                ))),
+            DiagnosticsPlugin,
+            LogPlugin::default(),
+            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin::default(),
+            EntityCountDiagnosticsPlugin::default(),
+            SystemInformationDiagnosticsPlugin,
+        ))
+        .insert_resource(Time::<Fixed>::from_seconds(FIXED_EVENT_LOOP))
+        .run();
 }
